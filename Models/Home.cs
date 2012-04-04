ec997c4dee0494c815d097a8a1a57766cc9da9e4
Jonathan Channon <jonathan.channon@gmail.com>
2012-04-04 21:46:11 +0100

    Basic Solution with hello world view

---
+++ Models/Home.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DinnerParty.Models
+{
+    public class Home
+    {
+        public string Header { get; set; }
+        public string Body { get; set; }
+    }
+}
---
+++ Modules/HomeModule.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Nancy;
+using DinnerParty.Models;
+
+namespace DinnerParty.Modules
+{
+    public class HomeModule : NancyModule
+    {
+        public HomeModule()
+        {
+            Get["/"] = parameters =>
+            {
+                var model = new Home() { Header = "Welcome to DinnerParty", Body = "A Nerddinner port" };
+
+                return View["Index", model];
+            };
+        }
+    }
+}
